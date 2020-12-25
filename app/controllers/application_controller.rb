4ed19051a85c0f9b61a9172dd7e370b0ba438c8c
makurano <ginsyari1@gmail.com>
2020-12-25 19:28:05 +0900

    新規登録画面/正規表現実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :last_name_en, :first_name, :first_name_en, :birthday])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 半角英数字混合
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  with_options presence: true do
+    # 全角での入力「あ〜漢字」正規表現
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
+      validates :last_name
+      validates :first_name
+    end
+    # 英語表記（頭文字が大文字）の正規表現
+    with_options format: { with: /\A[A-Z][a-z]*\Z/, message: 'is invalid. Input english(uppercase is letters)' } do
+      validates :last_name_en
+      validates :first_name_en
+    end
+    validates :birthday
+  end
+
+  has_many         :articles
+  has_one_attached :image
 end
