05434a49491d1c7cf1a895367ee46916593faa3c
makurano <ginsyari1@gmail.com>
2020-12-27 23:53:51 +0900

    Articleモデル記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  has_one_attached :article_file, dependent: :destroy
+
+  with_options presence: true do
+    validates :title
+    validates :category_id, numericality: { ohter_than: 0, message: 'select'}
+  end
 end
