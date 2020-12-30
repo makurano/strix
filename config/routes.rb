e7f4462b2567d24ffa648f523b03c67403f73d0a
makurano <ginsyari1@gmail.com>
2020-12-30 11:55:26 +0900

    ウィザード形式ログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
   resources :articles, only: %i[index new create show edit update destroy] do
     collection do
       get 'search'
     end
   end
-  resources :users, only: [:show]
+  resources :profiles, only: [:show]
 end
