5a5c0fafd3f2d14e16eba32249d22dfec3362306
makurano <ginsyari1@gmail.com>
2021-01-11 03:27:46 +0900

    profileモデル単体テストコード記述

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :profile do
-    
+    degree                { Faker::Educator.degree }
+    affiliation           { Faker::Educator.university }
+    research_fields       { Faker::Educator.subject }
+    contact               { Faker::Internet.free_email }
+    education             { Faker::Educator.university }
+    academic_affiliations { Faker::Educator.subject }
+    association :user
+
+    after(:build) do |profile|
+      profile.image.attach(io: File.open('public/images/soseki.jpg'), filename: 'soseki.jpg')
+    end
   end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @profile = FactoryBot.build(:profile)
+  end
+
+  describe 'プロフィール登録' do
+    context 'プロフィールが登録できるとき' do
+      it '全ての情報が入力されていれば登録できる' do
+        expect(@profile).to be_valid
+      end
+      it 'degreeが空でも登録できる' do
+        @profile.degree = ""
+        expect(@profile).to be_valid
+      end
+      it 'affiliationが空でも登録できる' do
+        @profile.affiliation = ""
+        expect(@profile).to be_valid
+      end
+      it 'research_fieldsが空でも登録できる' do
+        @profile.research_fields = ""
+        expect(@profile).to be_valid
+      end
+      it 'contactが空でも登録できる' do
+        @profile.contact = ""
+        expect(@profile).to be_valid
+      end
+      it 'educationが空でも登録できる' do
+        @profile.education = ""
+        expect(@profile).to be_valid
+      end
+      it 'academic_affiliationsが空でも登録できる' do
+        @profile.academic_affiliations = ""
+        expect(@profile).to be_valid
+      end
+      it 'imageが空でも登録できる' do
+        @profile.image = nil
+        expect(@profile).to be_valid
+      end
+    end
+
+    context 'プロフィールが登録できないとき' do
+    end
+  end
 end
