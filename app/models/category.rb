fd05dea9ba1a6b100c4a29b06afc69057dac7e3a
makurano <ginsyari1@gmail.com>
2021-01-18 19:56:20 +0900

    項目名等ビュー修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: 'paper' },
-    { id: 2, name: 'note' },
-    { id: 3, name: 'document' },
-    { id: 4, name: 'other' }
+    { id: 1, name: '論文 / paper' },
+    { id: 2, name: '研究ノート / note' },
+    { id: 3, name: '資料 / document' },
+    { id: 4, name: 'その他 / other' }
   ]
 
   include ActiveHash::Associations
